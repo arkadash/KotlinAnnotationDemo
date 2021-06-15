72aa9a94d4a102484428ab70d9519af392844ccb
Moshe Arkadash <moshe.arkadash@fiverr.com>
2021-06-15 23:16:10 +0300

    create file

--- sdk-generator/src/main/kotlin/Generator.kt
+++ sdk-generator/src/main/kotlin/Generator.kt
@@ -1,7 +1,8 @@
 import com.google.auto.service.AutoService
+import com.squareup.kotlinpoet.FileSpec
 import me.arkadash.example.MyConstant
+import java.io.File
 import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
@@ -9,10 +10,6 @@ import javax.lang.model.element.TypeElement
 
 @AutoService(Processor::class)
 class Generator: AbstractProcessor() {
-
-    override fun init(processingEnv: ProcessingEnvironment?) {
-        super.init(processingEnv)
-    }
 
     override fun getSupportedSourceVersion(): SourceVersion {
         return SourceVersion.latestSupported()
@@ -23,6 +20,22 @@ class Generator: AbstractProcessor() {
     }
 
     override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
-        TODO("Not yet implemented")
+        val elementsWithAnnotation = roundEnv?.getElementsAnnotatedWith(MyConstant::class.java)
+
+        createFile()
+        return true
+    }
+
+    private fun createFile() {
+        val packageName = "me.arkadash.sdk-genereted"
+        val fileName = "GeneratedConstants"
+        val file = FileSpec.builder(packageName, fileName).build() // KotlinPoet
+        val generatedDirectory = processingEnv.options[KAPT_GENERATED_NAME]
+
+        file.writeTo(File(generatedDirectory, "$fileName.kt"))
+    }
+
+    companion object {
+        const val KAPT_GENERATED_NAME = "kapt.kotlin.generated"
     }
 }
